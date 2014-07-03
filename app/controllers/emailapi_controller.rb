57f5b2e417f12f1b151514f72b5eff7de3e0b983
lrakhman <lana.rakhman@gmail.com>
2014-07-02 20:23:11 -0500

    first commit

---
+++ app/controllers/emailapi_controller.rb
@@ -0,0 +1,26 @@
+class EmailapiController < ApplicationController
+
+	def index
+	end
+
+	def subscribe
+
+		@list_id = "3a35672a90"
+		gb = Gibbon::API.new
+
+		gb.lists.subscribe({
+			:id => @list_id,
+			:email => {:email => params[:email][:address]}
+			})
+	end
+
+
+	# gb.campaigns.create({type: "regular", 
+	# 		options: {list_id: list_id, 
+	# 		subject: "Gibbon is cool", 
+	# 		from_email: "4caad4lyfe@gmail.com", 
+	# 		from_name: "Darth Vader", 
+	# 		generate_text: true}, 
+	# 		content: {html: "<html><head></head><body><h1>Foo</h1><p>Bar</p></body></html>"}})
+
+end
---
+++ config/initializers/gibbon.rb
@@ -0,0 +1,3 @@
+Gibbon::API.api_key = "0c70fc0b31a72bcb883a8d375d727ee0-us8"
+Gibbon::API.timeout = 15
+Gibbon::API.throws_exeptions = false
